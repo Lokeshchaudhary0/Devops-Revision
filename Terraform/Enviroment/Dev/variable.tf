a514b2a134967b6a2ded0da430d9bf871f6e0279
Loukesh Kumar <loukesh.chaudhary@gmail.com>
2025-11-09 02:27:54 +0530

    update code

--- Terraform/Enviroment/Dev/variable.tf
+++ Terraform/Enviroment/Dev/variable.tf
@@ -1,16 +1,43 @@
-variable "vnet" {}
-variable "rgs" {}
-variable "subnet" {}
-variable "storage" {}
-variable "vms" {}
-variable "nic" {}
-variable "acr" {}
-variable "aks" {}
-variable "sql_server" {}
-variable "db_server" {}
-# variable "sql_server_map" {
-#   description = "sqlserver1"
-#   type        = string
+variable "vnet" {
+  type = map(any)
+}
+
+variable "rgs" {
+type = map(any)
+}
+
+# variable "subnet" {
+#   type = map(any)
 # }
 
-variable "kv" {}
+variable "storage" {
+  type = map(any)
+}
+
+variable "vms" {
+  type = map(any)
+}
+
+variable "nic" {
+  type = map(any)
+}
+
+variable "acr" {
+ type = map(any)
+}
+
+variable "aks" {
+  type = map(any)
+}
+
+variable "sql_server" {
+  type = map(any)
+}
+
+variable "db_server" {
+  type = map(any)
+}
+
+variable "kv" {
+  type = map(any)
+}
