317474192131a1d71dff045ad5e9bde02d421058
KUMAR Loukesh (CNH) <f97202c@cnh1.cnhgroup.cnh.com>
2025-11-08 15:42:17 +0530

    update code

--- Terraform/Enviroment/Dev/main.tf
+++ Terraform/Enviroment/Dev/main.tf
@@ -36,23 +36,23 @@ module "aks" {
   aks        = var.aks
 }
 
-module "db_server" {
-  depends_on = [module.sql_server]
-  source     = "../../Modules/azurerm_azure_sql_db_server"
-  sql_server_map = module.sql_server.sql_server_map
-  db_server  = var.db_server
+# module "db_server" {
+#   depends_on = [module.sql_server]
+#   source     = "../../Modules/azurerm_azure_sql_db_server"
+#   sql_server_map = module.sql_server.sql_server_map
+#   db_server  = var.db_server
 
-}
-module "sql_server" {
-  depends_on = [module.rg]
-  source     = "../../Modules/azurerm_azure_sql_server"
-  sql_server = var.sql_server
+# }
+# module "sql_server" {
+#   depends_on = [module.rg]
+#   source     = "../../Modules/azurerm_azure_sql_server"
+#   sql_server = var.sql_server
 
-}
-module "kv" {
-  source = "../../Modules/azurerm_key_vault"
-  kv     = var.kv
-}
+# }
+# module "kv" {
+#   source = "../../Modules/azurerm_key_vault"
+#   kv     = var.kv
+# }
 resource "azurerm_role_assignment" "aks_acr_attach" {
   principal_id         = module.aks.aks_principal_id["aks1"]
   role_definition_name = "AcrPull"
