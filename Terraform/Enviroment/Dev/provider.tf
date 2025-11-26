c0b8dc8e89a3ae0577e2bf95376a15be36f79747
KUMAR Loukesh (CNH) <f97202c@cnh1.cnhgroup.cnh.com>
2025-11-26 17:59:35 +0530

    update code

--- Terraform/Enviroment/Dev/provider.tf
+++ Terraform/Enviroment/Dev/provider.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      source = "hashicorp/azurerm"
+      version = "4.54.0"
     }
   }
 }
